d00bbabe66dc1c060c5d8a63b57a3f0979604238
root <root@localhost.localdomain>
2025-10-02 22:34:20 +0100

    update

--- src/pages/crm/opportunities/index.tsx
+++ src/pages/crm/opportunities/index.tsx
@@ -9,6 +9,15 @@ import { GetHierarchyData } from "@utils/users";
 import { Column } from "@components/CustomDataTable";
 import { Button, Modal, Row, Col, Badge, Dropdown, Form } from "react-bootstrap";
 import { toast } from "react-toastify";
+import "@assets/scss/common.scss";
+import "@assets/scss/tabs.scss";
+import PageHeader from "@components/PageHeader";
+import FormModal from "../../partial/FormModal";
+import ConfirmModal from "@pages/partial/ConfirmModal";
+import SuccessfulModal from "@pages/partial/SuccessfulModal";
+import PageSummaryGrid, { SummaryCard } from '@components/PageSummaryGrid';
+import DatatableActionButton from "@components/DatatableActionButton";
+
 
 
 import {
