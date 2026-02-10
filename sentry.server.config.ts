a69c9829348310a9a00316b08dd8363dc6cbc988
Ali Bahadar <raoalibahadar@hotmail.com>
2026-02-10 14:25:58 +0000

    fix: sentry integration issues

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "http://1ac2e06e89bdd4144a6946332d1bede4@sentry.primealley.com:7800/3",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || process.env.SENTRY_ENVIRONMENT || process.env.SENTRY_ENV || process.env.NODE_ENV,
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import "@assets/scss/custom.scss";
 import "@assets/fonts/phosphor/duotone/style.css";
-import React, { ReactElement, ReactNode } from "react";
+import React, { Component, ReactElement, ReactNode } from "react";
 import Head from "next/head";
 import { wrapper } from "@toolkit/index";
 
@@ -8,6 +8,7 @@ import { AppProps } from "next/app";
 import type { NextPage } from "next";
 import { appWithTranslation } from "next-i18next";
 import { ToastContainer } from 'react-toastify';
+import * as Sentry from "@sentry/nextjs";
 import Providers from "@components/providers";
 import favicon from "@assets/images/favicon.png";
 import Router, { useRouter } from "next/router";
@@ -16,6 +17,58 @@ import "nprogress/nprogress.css";
 import ChatbotWidget from "@components/chatbot";
 import { useSession } from "next-auth/react";
 import { usePermissions } from "@utils/permissionUtils";
+
+// Error boundary that reports client-side render errors to Sentry
+class SentryErrorBoundary extends Component<
+  { children: ReactNode },
+  { hasError: boolean }
+> {
+  constructor(props: { children: ReactNode }) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    Sentry.captureException(error, {
+      extra: {
+        componentStack: errorInfo.componentStack,
+      },
+    });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{
+            padding: "2rem",
+            textAlign: "center",
+            minHeight: "200px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <h2>Something went wrong</h2>
+          <p>We’ve been notified and are looking into it.</p>
+          <button
+            type="button"
+            onClick={() => this.setState({ hasError: false })}
+            style={{ marginTop: "1rem", padding: "0.5rem 1rem", cursor: "pointer" }}
+          >
+            Try again
+          </button>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
 
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -73,7 +126,9 @@ const MyApp: any = ({ Component, pageProps, ...rest }: AppPropsWithLayout) => {
         <title>Business Workspace AI-Powered</title>
       </Head>
       <Providers store={store}>
-        <AppContent Component={Component} pageProps={pageProps} getLayout={getLayout} />
+        <SentryErrorBoundary>
+          <AppContent Component={Component} pageProps={pageProps} getLayout={getLayout} />
+        </SentryErrorBoundary>
       </Providers>
       <ToastContainer />
     </>
