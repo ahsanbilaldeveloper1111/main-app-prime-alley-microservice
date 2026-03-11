4534de90d4a0f682d76fe4da24dc1d86687acb4a
Ali Bahadar <raoalibahadar@hotmail.com>
2026-03-11 21:43:15 +0000

    refactor: clean up Accounts page by removing unused imports and state management
    - Removed unnecessary imports and state management related to session handling and loading/error states from the Accounts component.
    - Streamlined the component to focus on the Coming Soon notification, improving code clarity and maintainability.

--- src/pages/voicebot/outbound/analytics/indesx.tsx
+++ src/pages/voicebot/outbound/analytics/indesx.tsx
@@ -1,21 +1,9 @@
 import '@assets/scss/datatable-style.scss';
-import { ReactElement, useState } from 'react';
+import { ReactElement } from 'react';
 import Layout from '@layout/index';
-import BreadcrumbItem from '@common/BreadcrumbItem';
-import { Button, Form, InputGroup, Modal, Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
-import { toast } from 'react-toastify';
-import { useSession } from 'next-auth/react';
 import ComingSoon from '@components/ComingSoon';
 
-
 const Accounts = () => {
-    const { data:session, status } = useSession();
-   
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-   
-    
     return (
         <ComingSoon
             title="Coming Soon"
