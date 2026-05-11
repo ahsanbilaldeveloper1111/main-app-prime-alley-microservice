a9c53a6de2c9c97c5cad422de1d31eff2637ab42
daniyalwaseem11 <daniyalwaseem11@gmail.com>
2026-05-11 19:19:53 +0500

    Sonar issue fix

--- src/pages/chat/ai-faqs/global/components/AIFaqsGlobalPageView.tsx
+++ src/pages/chat/ai-faqs/global/components/AIFaqsGlobalPageView.tsx
@@ -25,7 +25,6 @@ export function AIFaqsGlobalPageView({ ctx }: AIFaqsGlobalPageViewProps) {
     showEditModal,
     setShowEditModal,
     showDeleteModal,
-    setShowDeleteModal,
     selectedFAQ,
     setSelectedFAQ,
     faqItems,
