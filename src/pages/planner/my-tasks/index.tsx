f2cbb1a77618c4c2e31228c0fbb0dd77a40f82ee
asadali.dev <asadali.dev@gmail.com>
2026-05-12 10:14:30 +0100

    Refactor PlannerMyTasksPage to streamline rendering
    - Simplified the PlannerMyTasksPage component by directly rendering MyDayTasksPage, removing unnecessary layout wrapper code for improved clarity and efficiency.

--- src/pages/planner/my-tasks/index.tsx
+++ src/pages/planner/my-tasks/index.tsx
@@ -2,13 +2,7 @@ import React, { ReactElement } from "react";
 import Layout from "@layout/index";
 import MyDayTasksPage from "../../../components/planner/MyDayTasksPage";
 
-const PlannerMyTasksPage: React.FC = () => {
-  return (
-    <Layout>
-    <div className="page-container"></div>
-    </Layout>
-  );
-};
+const PlannerMyTasksPage: React.FC = () => <MyDayTasksPage />;
 
 PlannerMyTasksPage.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
 
